9797f9cf8a4c6b47ae49cb7f2f176b90c778ede8
Evgeniy Tsvigun <utgarda@gmail.com>
2012-10-11 14:05:47 +0400

    Initial commit: Rails app structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Smorodina::Application.config.secret_token = 'c31d8ea3bd9434cea3e678adfdffafe1de2c2682ebdb7bde52f3d0666bc2c0b8f583cebcb3dbcdc228a88c4b6a1da02b64535dc908da0c4597bdc04a9f85db1e'
