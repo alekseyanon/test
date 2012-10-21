6eb872c1861d0c58d4745a78478060b43495aea8
Evgeniy Tsvigun <utgarda@gmail.com>
2012-10-21 20:47:22 +0400

    Tags as nested sets

---
+++ db/migrate/20121021144058_create_categories.rb
@@ -0,0 +1,16 @@
+class CreateCategories < ActiveRecord::Migration
+  def change
+    create_table :categories do |t|
+      t.string :name
+      t.string :description
+
+      # awesome_nested_set
+      t.integer :parent_id
+      t.integer :lft
+      t.integer :rgt
+      t.integer :depth
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/models/category_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+describe Category do
+  subject { described_class.make! }
+  it { should be_valid }
+  it { should validate_presence_of :name }
+  it { should validate_presence_of :description }
+  it 'acts as nested set' do
+    child = described_class.make!
+    child.move_to_child_of subject
+    child.parent.should == subject
+    subject.children.should == [child]
+  end
+end
