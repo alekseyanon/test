1a2920f7485cd2446d31a87d18ae7b7579fd58f9
Dmitry Gautsel <lvl0nax@gmail.com>
2012-10-31 14:31:05 +0400

    user model spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,19 +3,21 @@ require 'spec_helper'
 describe User do
   subject { described_class.make! }
   let (:user) { subject}
+  
   it { should be_valid }
   # it { should validate_presence_of :name }
   # it { should validate_presence_of :description }
   it { should have_many(:authentications) }
 
-  it "should not allow to save user without any role" #do
-    # user.roles = []
-    # user.should_not be_valid
-  #end
+  it "should not allow to save user without any role" do
+    user.roles = []
+    user.should_not be_valid
+  end
 
 	it "should not allow incorrect emails" do
     user.email = 'foo bar'
     user.should_not be_valid
   end
 
+  it {user.traveler?.should be_true}
 end
