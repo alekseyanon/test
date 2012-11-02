604fd020d12fd77dbe8183cae4a10453180b61c1
Evgeniy Tsvigun <utgarda@gmail.com>
2012-11-02 20:00:53 +0400

    pg_search used in Article

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,4 +3,7 @@ class Article < ActiveRecord::Base
   attr_accessible :body, :published, :published_at, :title
   validates :title, :body, :user, :presence => true
   validates_associated :user
+
+  include PgSearch
+  multisearchable :against => [:title, :body]
 end
