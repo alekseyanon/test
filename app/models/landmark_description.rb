6f1c2c677bcf8a2aff1625740f0a5849250989a5
Evgeniy Tsvigun <utgarda@gmail.com>
2012-11-21 19:14:00 +0400

    Text search for LandmarkDescription

--- app/models/landmark_description.rb
+++ app/models/landmark_description.rb
@@ -1,5 +1,24 @@
 class LandmarkDescription < Article
   belongs_to :landmark, :class_name => Geo::Landmark
+  has_many :tags, through: :landmark
+  has_one :node, through: :landmark
+
   validates_associated :landmark
   accessible_attributes :landmark_id
+
+  pg_search_scope :text_search,
+                  :against => {
+                      title: 'A',
+                      body: 'B'
+                  }
+
+  def self.search(query)
+    query ?
+        query.is_a?(Hash) ?
+            query.has_key?(:geom) ?
+                text_search(query[:text]).within_radius(query[:geom]) :
+                text_search(query[:text]) :
+            text_search(query) :
+        all
+  end
 end
