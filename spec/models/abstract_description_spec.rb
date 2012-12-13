5cc8ffe3cad866619e1e010e705086c6bd887e0d
Leonid <leonid@dvoretskiy.net>
2012-12-13 16:38:48 +0400

    Tests for AreaDescription

--- spec/models/abstract_description_spec.rb
+++ spec/models/abstract_description_spec.rb
@@ -1,5 +1,7 @@
 require 'spec_helper'
 
+#TODO cover weighting
+#TODO cover multi-word search
 describe AbstractDescription do
   subject { described_class.make! }
   it_behaves_like "an abstract description"
