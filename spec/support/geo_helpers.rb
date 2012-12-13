d0e71b58480c79b1897940e94d31d2573e542277
Leonid <leonid@dvoretskiy.net>
2012-12-13 14:17:28 +0400

    Specs fix

--- spec/support/geo_helpers.rb
+++ spec/support/geo_helpers.rb
@@ -18,3 +18,21 @@ end
 def landmarks_to_descriptions(landmarks)
   landmarks.map{|lm| LandmarkDescription.make! describable: lm}
 end
+
+def get_foursquares(start_from)
+  polygons = []
+  start_from.each do |coord|
+    polygons << Osm::Poly.make!(
+      geom: Geo.factory.polygon(
+        Geo.factory.line_string(
+          [
+            Geo.factory.point(coord[0], coord[1]),
+            Geo.factory.point(coord[0]+10, coord[1]),
+            Geo.factory.point(coord[0]+10, coord[1]+10),
+            Geo.factory.point(coord[0], coord[1]+10),
+            Geo.factory.point(coord[0], coord[1])
+          ]
+        )))
+  end
+  polygons
+end
