94d9c172699895bc47031ee068a722c4d231ad2d
Leonid <leonid@dvoretskiy.net>
2012-12-24 14:06:09 +0400

    Events navigator

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,10 +1,18 @@
 class EventsController < InheritedResources::Base
+  
   def new
     @landmarks = Landmark.limit(10)
     new!
   end
+
   def index
-    @event_occurrences = EventOccurrence.for_week
+    if params[:week]
+      @date = Time.parse(params[:week])
+    else
+      @date = Time.now
+    end
+    @event_occurrences = EventOccurrence.for_week 
     index!
   end
+  
 end
