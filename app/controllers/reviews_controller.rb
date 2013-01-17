de99ca3809a1eb43dea7bfe098438fdd443e8beb
Leonid <leonid@dvoretskiy.net>
2013-01-17 13:42:55 +0400

    nested paths

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,13 @@
 class ReviewsController < InheritedResources::Base
+  def new
+    @landmark_description = LandmarkDescription.find params[:landmark_description_id]
+    @review = @landmark_description.reviews.build
+    new!
+  end
+
+  def create
+    @landmark_description = LandmarkDescription.find params[:landmark_description_id]
+    @review = @landmark_description.reviews.create params[:review]
+    redirect_to @landmark_description
+  end
 end
