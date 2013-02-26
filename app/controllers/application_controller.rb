b7c29850897b5fd593ef4f98efa78906f5730e4a
lvl0nax <lvl0nax@gmail.com>
2013-02-26 17:46:59 +0400

    change logger method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   def logg str, variable
-    logger.debug "========================================================="
-    logger.debug str
-    logger.debug variable
-    logger.debug "========================================================="
+    logger.debug "#{Time.now} | url - #{request.original_url} | " + "#{variable} | " + str
   end
 end
