5f0b782463813f590bdf1a3b462e25a2800f96a0
lvl0nax <lvl0nax@gmail.com>
2013-02-27 12:38:06 +0400

    change variables name

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,8 @@
 module ApplicationHelper
-  def link_to_vote text, controller = ''
-    link_to "#{text.split('_')[0]}", "#", id: "vote-#{text.split('_')[0]}", onclick: "to_vote(\'#{controller}#{text.split('_')[1]}\', #{text.split('_')[2]}, \'#{text.split('_')[0]}\');"
+  def link_to_vote css_id, controller = ''
+    sign = css_id.split('_')[0]
+    model = css_id.split('_')[1]
+    id = css_id.split('_')[2]
+    link_to "#{sign}", "#", id: "vote-#{sign}", onclick: "to_vote(\'#{controller}#{model}\', #{id}, \'#{sign}\');"
   end
 end
