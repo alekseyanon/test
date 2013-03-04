48cd5494ed9105545950d59b43e2f16aec1fbcda
lvl0nax <lvl0nax@gmail.com>
2013-03-04 14:35:58 +0400

    delete 2 string from config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,8 +16,6 @@ Smorodina::Application.configure do
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = true
 
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.default :charset => "utf-8"
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
