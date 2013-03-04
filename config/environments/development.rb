0cd13766db2f01552c0fcaee053462cd0fb8d307
lvl0nax <lvl0nax@gmail.com>
2013-03-04 14:34:48 +0400

    identical with dev main

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -21,13 +21,13 @@ Smorodina::Application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address: 'smtp.gmail.com',
+    address: "smtp.gmail.com",
     port: 587,
-    domain: 'gmail.com',
-    authentication: 'plain',
+    domain: "gmail.com",
+    authentication: "plain",
     enable_starttls_auto: true,
-    user_name: 'monax.spam@gmail.com',
-    password: 'code4gmail'
+    user_name: "monax.spam@gmail.com",
+    password: "code4gmail"
   }
   config.action_mailer.default_url_options = {host: "localhost:3000"}
 
