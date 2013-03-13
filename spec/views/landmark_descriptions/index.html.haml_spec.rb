30210b1919c26ff2a4d75f92a7c1c5a574b49abf
lvl0nax <lvl0nax@gmail.com>
2013-03-13 11:26:30 +0400

    fix views index landmark description test

--- spec/views/landmark_descriptions/index.html.haml_spec.rb
+++ spec/views/landmark_descriptions/index.html.haml_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe "landmark_descriptions/index" do
   before(:each) do
-    assign :landmark_descriptions, Array.new(2){ |i| LandmarkDescription.make! }
+    assign :landmark_descriptions, Kaminari.paginate_array( Array.new(2){ |i| LandmarkDescription.make!}).page(1)
   end
 
   it "renders a list of landmark_descriptions" do
