c79c5675726915de2ecf4f75004adc532bbd7ead
Evgeniy Tsvigun <utgarda@gmail.com>
2013-03-20 20:35:21 +0400

    Nodes sequences concatenation stored procedure

--- db/sql/myltipoly.sql
+++ db/sql/myltipoly.sql
@@ -6,4 +6,25 @@ select * from relations
        ) as simple_multipoly
     on relations.id = simple_multipoly.relation_id
 where tags->'type' = 'multipolygon'
-  and tags->'boundary' = 'administrative'
+  and tags->'boundary' = 'administrative';
+
+CREATE OR REPLACE FUNCTION concatenate_ways(multipoly bigint)
+  RETURNS geometry[]
+AS $$
+    if not SD.has_key("nodes_plan"):
+        SD["nodes_plan"] = plpy.prepare("select nodes from ways where ways.id in ( select member_id from relation_members where relation_id = $1)", ["bigint"])
+        SD["geoms_plan"] = plpy.prepare("select geom from nodes where id = $1", ["bigint"])
+    nodes_plan = SD["nodes_plan"]
+    geoms_plan = SD["geoms_plan"]
+
+    res = plpy.execute(nodes_plan, [multipoly])
+    sequences = [r["nodes"] for r in res]
+    head, tail = sequences[0], sequences[1:]
+    while len(tail) > 0:
+        nxt = next(seq for seq in tail if (seq[0] == head[-1] or seq[-1] == head[-1]))
+        tail.remove(nxt)
+        if nxt[-1] == head[-1]:
+            nxt.reverse()
+        head += nxt[1:]
+    return [plpy.execute(geoms_plan, [id])[0]["geom"] for id in head]
+$$ LANGUAGE plpythonu;
