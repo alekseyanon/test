99a33a7d8f8e83088baa9112aedba8a83e5d9a4f
Evgeniy Tsvigun <utgarda@gmail.com>
2013-03-24 20:57:22 +0400

    relations table updated with transformed geometry

--- db/sql/myltipoly.sql
+++ db/sql/myltipoly.sql
@@ -37,3 +37,19 @@ AS $$
     except:
         return None
 $$ LANGUAGE plpythonu;
+
+UPDATE relations
+SET geom = ST_MakePolygon(ST_MakeLine(concatenate_ways(id)))
+WHERE id IN (
+  SELECT id FROM relations
+    JOIN (
+           SELECT relation_id FROM relation_members AS M
+           GROUP BY relation_id
+           HAVING 'outer' = ALL (SELECT member_role FROM relation_members
+           WHERE relation_members.relation_id = M.relation_id)
+         ) AS simple_poly
+      ON relations.id = simple_poly.relation_id
+  WHERE tags -> 'type' = 'multipolygon'
+    AND tags -> 'boundary' = 'administrative'
+     OR tags ? 'place'
+);
