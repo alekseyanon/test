ded5f5cf92514ac6dde8445d86cedc6baf2fa008
Leonid <leonid@dvoretskiy.net>
2013-04-08 15:57:43 +0400

    Rewrite spec name

--- spec/controllers/api/objects_controller_spec.rb
+++ spec/controllers/api/objects_controller_spec.rb
@@ -4,16 +4,16 @@ describe Api::ObjectsController do
 
   describe "GET 'nearby'" do
 
-    it "TODO print somthing" do
+    it 'returns objects nearby another' do
       n1 = Osm::Node.make! geom: 'POINT(1 1)'
       l1 = Landmark.make! osm: n1
-      ld1 = LandmarkDescription.make! describable: l1, title: 1
+      ld1 = LandmarkDescription.make! describable: l1, title: 1  # Наш технический долг касательно БД
       n2 = Osm::Node.make! geom: 'POINT(2 2)'
       l2 = Landmark.make! osm: n2
-      ld2 = LandmarkDescription.make! describable: l2, title: 2
+      ld2 = LandmarkDescription.make! describable: l2, title: 2  # дает о себе знать даже здесь
       n3 = Osm::Node.make! geom: 'POINT(10 10)'
       l3 = Landmark.make! osm: n3
-      ld3 = LandmarkDescription.make! describable: l3, title: 3
+      ld3 = LandmarkDescription.make! describable: l3, title: 3  # в тестах
       get :nearby, id: ld1.id, r: 5
       assigns(:objects).should == [ld2]
     end
