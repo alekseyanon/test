7cef1b4a4e4f4941c39746e4f69dd18599f24613
Leonid <leonid@dvoretskiy.net>
2013-04-11 15:51:07 +0400

    Add object to spec

--- spec/controllers/api/objects_controller_spec.rb
+++ spec/controllers/api/objects_controller_spec.rb
@@ -14,8 +14,11 @@ describe Api::ObjectsController do
       n3 = Osm::Node.make! geom: 'POINT(10 10)'
       l3 = Landmark.make! osm: n3
       ld3 = LandmarkDescription.make! describable: l3, title: 3  # в тестах
+      n4 = Osm::Node.make! geom: 'POINT(0 0)'
+      l4 = Landmark.make! osm: n4
+      ld4 = LandmarkDescription.make! describable: l4, title: 4
       get :nearby, id: ld1.id, r: 5
-      assigns(:objects).should == [ld2]
+      assigns(:objects).should =~ [ld2, ld4]
     end
 
   end
