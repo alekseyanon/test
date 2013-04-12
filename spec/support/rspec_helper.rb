2cf8ba3ad897f07ac5fb4ed7fc6878a8c29b0446
lvl0nax <lvl0nax@gmail.com>
2013-04-12 11:14:14 +0400

    rollback previous changes

--- spec/support/rspec_helper.rb
+++ spec/support/rspec_helper.rb
@@ -5,7 +5,6 @@ module RspecHelper
     visit new_user_session_path
     fill_in 'user_email', with: user.email
     fill_in 'user_password', with: user.password
-    #click_on 'Sign in'
-    js_click('.btn')
+    click_on 'Sign in'
   end
 end
