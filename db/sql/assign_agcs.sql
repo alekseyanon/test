7c9f88fd97982bc662c42e51694d81597399bda5
Evgeniy Tsvigun <utgarda@gmail.com>
2013-04-18 19:33:27 +0400

    Update assign_agcs.sql
    После мерджа новой базы

--- db/sql/assign_agcs.sql
+++ db/sql/assign_agcs.sql
@@ -1,9 +1,8 @@
 UPDATE geo_objects AS G
 SET agc_id = (
   SELECT A.id FROM agcs AS A
-    JOIN nodes ON nodes.id = G.osm_id
     JOIN relations
       ON A.relations [array_length(A.relations, 1)] = relations.id --TODO a shorter way to get last element?
-  WHERE ST_Contains(relations.geom, nodes.geom)
+  WHERE ST_Contains(relations.geom, G.geom)
   ORDER BY array_length(A.relations, 1) DESC
   LIMIT 1);
