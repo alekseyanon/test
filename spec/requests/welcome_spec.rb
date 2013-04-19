8e9572735ea25bf0de0d64193dbab0d6c7758427
Leonid <leonid@dvoretskiy.net>
2013-04-19 16:33:04 +0400

    Spec fix

--- spec/requests/welcome_spec.rb
+++ spec/requests/welcome_spec.rb
@@ -3,10 +3,19 @@ require 'spec_helper'
 
 describe "Welcome", js: true, type: :request do
 
+  self.use_transactional_fixtures = false
+
+  before :all do
+    setup_db_cleaner
+  end
+
+  after :all do
+    DatabaseCleaner.clean
+  end
+
   it 'should show objects count number' do
     GeoObject.make!
     visit root_path
-    sleep 5
     page.find('#objectsTotal').should have_content '1'
   end
 
