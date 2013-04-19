c4898d312b462ad2e443e1f54e95bc3b2a200e4f
Evgeniy Tsvigun <utgarda@gmail.com>
2013-04-19 19:16:27 +0400

    cosmetic

--- app/models/search_query.rb
+++ app/models/search_query.rb
@@ -3,14 +3,13 @@ class SearchQuery < ActiveRecord::Base
   validates :str, presence: true, uniqueness: true
 
   def self.add str
-    #REVIEW transaction is not necessary here?
     sq = self.find_or_create_by_str str.mb_chars.downcase.to_s
-    sq.update_column :counter, sq.counter+1
+    sq.increment! :counter
   end
 
   def self.popular_for str
     str = str.mb_chars.downcase.to_s
-    self.where("str LIKE '%#{str}%'").order('counter DESC').limit(10).map {|sq| sq.str }
+    self.where("str LIKE '%#{str}%'").order('counter DESC').limit(10).pluck :str
   end
 
 end
