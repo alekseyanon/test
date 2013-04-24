0cf82b2bfa9117561a07a78e7d37c6f4c7f24217
lvl0nax <lvl0nax@gmail.com>
2013-04-24 13:02:42 +0400

    add imageable to geo object without js uploading

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -9,6 +9,10 @@ class ImageUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
+  def extension_white_list
+     %w(jpg jpeg gif png)
+  end
+
   version :thumb do
     resize_to_fill(100, 100)
   end
