c5306c205370cd045fc861fbcfe6fd765276e604
lvl0nax <lvl0nax@gmail.com>
2013-04-25 12:37:08 +0400

    add images to geo_objects

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -45,6 +45,18 @@ $(function() {
       onChange: update
     });
 
+  $('form').on('click', '.remove_fields', function(event) {
+    $(this).closest('fieldset').hide();
+    return event.preventDefault();
+  });
+
+  $('form').on('click', '.add_fields', function(event) {
+    var regexp, time;
+    time = new Date().getTime();
+    regexp = new RegExp($(this).data('id'), 'g');
+    $(this).before($(this).data('fields').replace(regexp, time));
+    return event.preventDefault();
+  });
 });
 
 /*TODO сделать корректно.
