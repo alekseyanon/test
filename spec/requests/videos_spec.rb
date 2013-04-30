f79cdc1386472ca5ce506b400be418d95313850a
lvl0nax <lvl0nax@gmail.com>
2013-04-30 13:29:18 +0400

    fix specs and delete pending tests.

--- spec/requests/videos_spec.rb
+++ spec/requests/videos_spec.rb
@@ -18,9 +18,20 @@ describe 'Videos', js: true, type: :request do
     visit event_path(e)
     click_on 'Add video'
     page.current_path.should == new_event_video_path(e)
-    fill_in 'you_tude_url', with: 'http://www.youtube.com/watch?v=EYApe6Cs11A'
+    page.find('#you_tube_url').set 'http://www.youtube.com/watch?v=EYApe6Cs11A'
     click_on 'Save'
     visit event_path(e)
+    page.should have_selector('iframe[name="EYApe6Cs11A"]')
+  end
+
+  it 'able to attach to GeoObject' do
+    login
+    go = GeoObject.make!(tag_list: [Category.make!.name])
+    visit geo_object_path(go)
+    click_on 'Add video'
+    page.current_path.should == new_geo_object_video_path(go)
+    page.find('#you_tube_url').set 'http://www.youtube.com/watch?v=EYApe6Cs11A'
+    click_on 'Save'
     page.should have_selector('iframe[name="EYApe6Cs11A"]')
   end
 end
