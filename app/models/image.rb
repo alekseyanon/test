4474681157ebf65226d5522b0d80c2e0707e68bb
Leonid <leonid@dvoretskiy.net>
2013-05-07 09:13:10 +0400

    Image is commentable

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,7 +1,9 @@
 class Image < ActiveRecord::Base
   attr_accessible :image
   mount_uploader  :image, ImageUploader
-  belongs_to      :imageable, polymorphic: true
 
+  has_many 				:comments, as: :commentable
+  belongs_to      :imageable, polymorphic: true
   acts_as_voteable
+
 end
