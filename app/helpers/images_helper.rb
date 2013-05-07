a10a51873094219024c005304c94307926a5f95f
Leonid <leonid@dvoretskiy.net>
2013-05-07 11:10:39 +0400

    First an last

--- app/helpers/images_helper.rb
+++ app/helpers/images_helper.rb
@@ -8,4 +8,12 @@ module ImagesHelper
 		image.imageable.images.where('id < ?', image.id).order('id desc').limit(1).first
   end
 
+  def first_image image
+		image.imageable.images.order('id').limit(1).first
+  end
+
+  def last_image image
+		image.imageable.images.order('id desc').limit(1).first
+  end
+
 end
