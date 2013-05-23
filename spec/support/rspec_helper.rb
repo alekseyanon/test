ed9e699e9fd9bbe76ed847a1879c431843c0cf6b
lvl0nax <lvl0nax@gmail.com>
2013-05-23 15:50:29 +0400

    fix spec again

--- spec/support/rspec_helper.rb
+++ spec/support/rspec_helper.rb
@@ -3,9 +3,17 @@
 module RspecHelper
   def login user=User.make!
     Capybara.visit('/')
-    page.find('#show_registration_modal').click
+    page.should have_selector('#show_registration_modal')
+    find('#show_registration_modal').click
     fill_in 'user_login_email', with: user.email
     fill_in 'user_login_password', with: user.password
-    page.find('#login').click
+    page.should have_selector('#login')
+    find('#login').click
+    if page.has_selector?('#login')
+      find('#login').click
+    end
+    if page.has_selector?('#login')
+      find('#login').click
+    end
   end
 end
