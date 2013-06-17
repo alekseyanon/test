1a41546df607671047092219d495dd41c07b522d
lvl0nax <lvl0nax@gmail.com>
2013-06-17 18:07:32 +0400

    code format

--- app/controllers/api/chronicles_controller.rb
+++ app/controllers/api/chronicles_controller.rb
@@ -3,11 +3,9 @@ class Api::ChroniclesController < ApplicationController
     window = CHRONICLE_PAGINATION_ITEMS
     offset = params[:offset].to_i
     @objects = (newest = GeoObject.newest).limit(window).offset(offset)
-    unless @objects.blank?
-      unless @objects.group_by{|g| g.day_creation}.values.last.count % 2 == 0
-        go = newest.offset(offset+window).first
-        @objects.push go if go.try(:day_creation) == @objects.last.day_creation
-      end
+    if @objects.present? && @objects.group_by{|g| g.day_creation}.values.last.count.odd?
+      go = newest.offset(offset+window).first
+      @objects.push go if go.try(:day_creation) == @objects.last.day_creation
     end
     @offset = offset + @objects.size
   end
