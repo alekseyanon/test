43893aaae7de941a330038e864a0d698daac2ffc
lvl0nax <lvl0nax@gmail.com>
2013-06-27 14:08:59 +0400

    add spec for user ratings

--- app/controllers/api/ratings_controller.rb
+++ app/controllers/api/ratings_controller.rb
@@ -5,12 +5,17 @@ class Api::RatingsController < ApplicationController
                     experts:        'expert DESC',
                     discoverers:    'discoverer DESC'
   }
+
+  ### api url for user ratings
+  # /api/ratings/list.json?page=2
+  # /api/ratings/list.json?order_by=commentators
+  # /api/ratings/list.json?order_by=bloggers&page=2
   def list
     cond = if query = params[:order_by].try(:to_sym)
              @@type_to_sql[ query ]
            else
              '(commentator + blogger + photographer + expert + discoverer) DESC'
            end
-    @users = User.order(cond, 'created_at DESC').page params[:page]
+    @users = User.sorted_list_with_page(cond, params[:page].to_i)
   end
 end
