e70ad467272d53dfa288f48665f5194bdb593712
lvl0nax <lvl0nax@gmail.com>
2013-06-27 15:15:22 +0400

    fix warning for set variable in if condition

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -2,15 +2,15 @@ class RatingsController < ApplicationController
 
   respond_to :html, :json
 
-  @@type_to_sql = { commentators:   'commentator DESC',
+  TYPE_TO_SQL = { commentators:   'commentator DESC',
                   bloggers:       'blogger DESC',
                   photographers:  'photographer DESC',
                   experts:        'expert DESC',
                   discoverers:    'discoverer DESC'
   }
   def list
-    cond = if query = params[:order_by].try(:to_sym)
-             @@type_to_sql[ query ]
+    cond = if (query = params[:order_by].try(:to_sym))
+             TYPE_TO_SQL[ query ]
            else
              '(commentator + blogger + photographer + expert + discoverer) DESC'
            end
