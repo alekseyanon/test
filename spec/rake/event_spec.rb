d44ca340fad54e90c337b1285e4ba97fe550f1a0
Evgeniy Tsvigun <utgarda@gmail.com>
2013-07-11 20:32:22 +0400

    events specs fixed

--- spec/rake/event_spec.rb
+++ spec/rake/event_spec.rb
@@ -5,12 +5,11 @@ load File.join(Rails.root, 'Rakefile')
 
 describe 'Event rake tasks' do
 
-  let(:event) { Event.make!(start_date: 1.day.ago) }
+  let!(:event) { Event.make!(start_date: 1.day.ago) }
 
   it 'update event states' do
-    event
     Rake::Task['process_event_states'].invoke
-    Event.first.state.should == 'started'
+    event.state.should == 'started'
   end
 
 end
