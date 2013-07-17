60837863c117dd72f076a209d6aea32655371609
lvl0nax <lvl0nax@gmail.com>
2013-07-17 14:25:08 +0400

    fix complaint spec and api controller

--- app/controllers/api/complaints_controller.rb
+++ app/controllers/api/complaints_controller.rb
@@ -3,13 +3,17 @@ class Api::ComplaintsController < InheritedResources::Base
   before_filter :find_complaintable, only: [:new, :create]
   load_and_authorize_resource only: CRUD_ACTIONS
 
+  ### TODO: should be removed after implement API
   def new
     @complaintable.insert(-1, Complaint.new)
   end
 
+  ### TODO: should be changed after implement API
   def create
     @complaint = @complaintable.last.complaints.build(params[:complaint])
     @complaint.user = current_user
+    @complaint.save
+    render json: {success: :true}
   end
 
   private
